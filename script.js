8363db021d79ede9a990b9b17b355671c4c86f00
aroon4020 <kessaniaroon4020@gmail.com>
2023-09-06 14:54:39 +0500

    1st commit

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
+require('dotenv').config();
 const ethers = require("ethers");
-let abi = require('./contract/abi.json')
+let abi = require('./abi/settlement.json')
 let { SignTypedDataVersion, signTypedData, recoverTypedSignature, TypedDataUtils } = require("@metamask/eth-sig-util");
 const { default: axios } = require("axios");
 const pk = "";
+
 const createOrderData = {
     domain: {
         name: "Dafi-Protocol",
         version: "V1",
         chainId: 5, // Replace with the Hardhat's chain ID
-        verifyingContract: "0xF58cb7a5c027DaBcD135afAf23D04c0412643B1A",
+        verifyingContract: "0xc0B30fbe9A028C61D75eB92837aca06f741e0a38",
     },
-    // This defines the message you're proposing the user to sign, is dapp-specific, and contains
-    // anything you want. There are no required fields. Be as explicit as possible when building out
-    // the message schema.
     message: {
-        sellToken: "0x91056d4a53e1faa1a84306d4deaec71085394bc8",
-        buyToken: "0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6",
+        sellToken: "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6",
+        buyToken: "0x91056d4a53e1faa1a84306d4deaec71085394bc8",
         receiver: "0xdbfa076edbfd4b37a86d1d7ec552e3926021fb97",
-        sellAmount: "19000000000000000000",
-        buyAmount: "10000000000000",
-        validTo: "1692797427",
+        sellAmount: "10000000000000000",
+        buyAmount: "1",
+        account: "0xd1795B1E24B0DFA4F207b63a201790A247FA27a3",
+        validTo: "1694058002",
         partiallyFillable: false,
         feeAmount: "0"
     },
-    // This refers to the keys of the following types object.
+    
     primaryType: "Data",
     types: {
-        // This refers to the domain the contract is hosted on.
         EIP712Domain: [
             { name: "name", type: "string" },
             { name: "version", type: "string" },
@@ -40,6 +39,7 @@ const createOrderData = {
             { name: "receiver", type: "address" },
             { name: "sellAmount", type: "uint256" },
             { name: "buyAmount", type: "uint256" },
+            { name:  "account", type: "address" },
             { name: "validTo", type: "uint32" },
             { name: "partiallyFillable", type: "bool" },
             { name: "feeAmount", type: "uint256" },
@@ -59,20 +59,20 @@ let recoveredCreateAddress = recoverTypedSignature({
 })
 console.log("recovered order", recoveredCreateAddress);
 let main = async () => {
-    let provider = new ethers.providers.JsonRpcProvider(
-        'https://eth-goerli.g.alchemy.com/v2/uZQ-N-ACmJP2S72X4Ave9TOENtpdDKkW',
+    let provider = new ethers.JsonRpcProvider(
+        'https://eth-goerli.g.alchemy.com/v2/UhpuznU9sEL8E-127qaU1-4yuVP9Hy1w',
     );
     let wallet = new ethers.Wallet(pk, provider);
     let contract = new ethers.Contract(
-        '0xF58cb7a5c027DaBcD135afAf23D04c0412643B1A',
+        '0xc0B30fbe9A028C61D75eB92837aca06f741e0a38',
         abi,
         wallet,
     );
-    let signer = await contract.getHashGPV2(createOrderData.message, createOrderSignature)
-    console.log(signer)
-    // let tx=await contract.getHashGPV2
+    let tx = await contract.commitFullOrderForGPv2(createOrderData.message, createOrderSignature);
+    console.log(tx);
+    
     const offchain = {
-        from: "0xF58cb7a5c027DaBcD135afAf23D04c0412643B1A",
+        from: "0xd1795B1E24B0DFA4F207b63a201790A247FA27a3",
         sellToken: createOrderData.message.sellToken,
         buyToken: createOrderData.message.buyToken,
         receiver: createOrderData.message.receiver,
@@ -86,7 +86,7 @@ let main = async () => {
         sellTokenBalance: "erc20",
         buyTokenBalance: "erc20",
         signingScheme: "eip1271",
-        signature: createOrderSignature,
+        signature: "0x",
     };
     const response = await axios.post('https://api.cow.fi/goerli/api/v1/orders', offchain);
     console.log(response.data);
